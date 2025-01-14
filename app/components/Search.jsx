3f9722b63ef620ab91cc111ffb9b45a6f9c6645e
Ovesh Mulla <oveshmulla1124@gmail.com>
2025-01-14 17:23:30 +0530

    fix the seacrh bar

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation"; // Import useRouter from next/navigation
+import Image from "next/image";
 
 const Search = () => {
   const [query, setQuery] = useState("");
@@ -21,35 +22,29 @@ const Search = () => {
       }}
     >
       {/* Title */}
-      <div className="flex items-center flex-col w-full lg:w-[700px] space-y-6 lg:space-y-[34px] text-white text-center " >
+      <div className="flex items-center flex-col w-full lg:w-[700px] space-y-6 lg:space-y-[34px] text-white text-center ">
+        <h1 className="text-[36px] text-white leading-[42px] lg:text-[60px] lg:leading-[70px] font-bold">
+          Search for a client
+        </h1>
 
-  
-      <h1 className="text-[36px] text-white leading-[42px] lg:text-[60px] lg:leading-[70px] font-bold">
-       Search for a client
-      </h1>
+        <p className="text-[18px] text-white lg:text-[28px]">
+          Enter the client name to search for their details and queries. We make
+          it easy to manage all your client information in one place.
+        </p>
 
-      <p className="text-[18px] text-white lg:text-[28px]">
-        Enter the client name to search for their details and queries. We make
-        it easy to manage all your client information in one place.
-      </p>
-     
-
-      {/* Search Bar */}
-      <div className="flex   md:w-[70%]  items-center">
-        <input
-          type="text"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="Enter client name..."
-          className="flex-grow px-4 py-2 rounded-l-md border-none"
-        />
-        <button
-          onClick={handleSearch}
-          className="bg-light_blue text-dark_blue  px-6 py-2 rounded-r-md hover:bg-dark_blue hover:text-light_blue "
-        >
-          Search
-        </button>
-      </div>
+        {/* Search Bar */}
+        <div className="flex p-5 w-[300px] relative  md:w-[400px]  items-center">
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Enter client name..."
+            className="flex-grow  px-4 py-2 rounded-l-md border-none"
+          />
+          <button onClick={handleSearch} className="absolute  bg-light_blue rounded-lg right-8 top-8">
+            <Image src="/SVG/send.svg" alt="Send" width={18} height={18} />
+          </button>
+        </div>
       </div>
     </div>
   );
